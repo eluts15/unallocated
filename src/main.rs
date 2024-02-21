875d1c3cbafc54b3cad507f870a146848fc2c704
Ethan Luts <ethanl@4thdownsoftware.com>
2024-02-21 15:57:59 -0800

    pass in custom provider, .env rather than aws profile.

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,86 @@
 mod fetch_records;
 mod linode_api_read_only;
 mod list_ec2_ips;
+
 use crate::fetch_records::{list_all_resource_record_sets, search_hosted_zones};
 use crate::linode_api_read_only::{extract, list_linode_instances};
 use crate::list_ec2_ips::list_all_ec2_ips;
+
+use aws_config::BehaviorVersion;
+use aws_credential_types::provider::{ProvideCredentials, SharedCredentialsProvider};
+use aws_sdk_ec2::config::{Credentials, Region};
 use aws_sdk_ec2::Client as Ec2Client;
 use aws_sdk_route53::Client as Route53Client;
-//use clap::{value_parser, Arg, Command};
+
 use dotenv::dotenv;
 use serde_json::to_string_pretty;
 use std::env;
 
+//use log::{debug, error, info, log_enabled, Level};
+
+#[derive(Debug)]
+struct StaticCredentials {
+    access_key_id: String,
+    secret_access_key: String,
+}
+
+impl StaticCredentials {
+    pub fn new() -> Self {
+        dotenv().ok();
+        let access_key_id = env::var("AWS_ACCESS_KEY_ID").expect("access_key_id");
+        let secret_access_key = env::var("AWS_SECRET_ACCESS_KEY").expect("secret access key");
+
+        Self {
+            access_key_id: access_key_id.trim().to_string(),
+            secret_access_key: secret_access_key.trim().to_string(),
+        }
+    }
+
+    async fn load_credentials(&self) -> aws_credential_types::provider::Result {
+        Ok(Credentials::new(
+            self.access_key_id.clone(),
+            self.secret_access_key.clone(),
+            None,
+            None,
+            "StaticCredentials",
+        ))
+    }
+}
+
+impl ProvideCredentials for StaticCredentials {
+    fn provide_credentials<'a>(
+        &'a self,
+    ) -> aws_credential_types::provider::future::ProvideCredentials<'a>
+    where
+        Self: 'a,
+    {
+        aws_credential_types::provider::future::ProvideCredentials::new(self.load_credentials())
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Config
-    // AWS credential setup.
     dotenv().ok();
 
-    let token = env::var("LINODE_API_TOKEN").expect("LINODE_API_TOKEN in .env file.");
-    let env_profile = "PROFILE";
-    let profile_name = dotenv::var(env_profile).unwrap();
-    let shared_config = aws_config::from_env()
-        .credentials_provider(
-            aws_config::profile::ProfileFileCredentialsProvider::builder()
-                .profile_name(profile_name)
-                .build(),
-        )
-        .load()
-        .await;
+    // AWS credential setup.
+    let shared_config = aws_config::SdkConfig::builder()
+        .behavior_version(BehaviorVersion::latest())
+        .region(Region::new(
+            env::var("AWS_REGION").expect("AWS_REGION in .env file."),
+        ))
+        .credentials_provider(SharedCredentialsProvider::new(StaticCredentials::new()))
+        .build();
 
     let r53_client = Route53Client::new(&shared_config);
+
     let ec2_client = Ec2Client::new(&shared_config);
+
+    let token = env::var("LINODE_API_TOKEN").expect("LINODE_API_TOKEN in .env file.");
 
     match search_hosted_zones(&r53_client).await {
         Ok(zone_ids) => {
             // Fetch all "A" records.
             let a_records = list_all_resource_record_sets(&r53_client, &zone_ids).await;
-
             match a_records {
                 Ok(a_records) => {
                     println!("Fetching public IPs for associated Linode Instances.");
